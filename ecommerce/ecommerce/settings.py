b2dbe266c29aab0aa7874006b1665c92b3c16e1a
erenyurtcu <ierenyurtcu@gmail.com>
2024-12-06 18:50:29 +0300

    db changed
    db changed from sqlite to postgresql

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Amazon',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
